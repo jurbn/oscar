eb29f31e52fd982f6c01c156d5779a6d9e73fca5
jurbn <777295@unizar.es>
2022-10-05 18:08:28 +0200

    merge

--- helpers.py
+++ helpers.py
@@ -44,23 +44,37 @@ def pos_bot(vw, x_w_r, t):
         th = vw[1] * t
         al = (np.pi - th) / 2
         x_r_r2 = np.array([r*chord(th)*np.sin(al), r*chord(th)*np.cos(al), th])
-    x_w_r2 = loc(np.matmul(hom(x_w_r), hom(x_r_r2)))   # nueva localizacion x_w_r
+    x_w_r2 = loc(np.matmul(hom(x_w_r), hom(x_r_r2)))   # new location x_w_r
     return x_w_r2
+
 
 def hom(x: np.array):
     """
     Returns the Transformation matrix based on the location vector
     """
-    return np.matrix([[np.cos(x[2]), -np.sin(x[2]) , x[0]], [np.sin(x[2]), np.cos(x[2]), x[1]], [0, 0, 1]])
+    return np.matrix([[np.cos(x[2]), -np.sin(x[2]), x[0]], [np.sin(x[2]), np.cos(x[2]), x[1]], [0, 0, 1]])
+
 
 def loc(T):
     """
     Returns the location vector based on the transformation matrix
     """
-    return np.array([T.item(0, 2), T.item(1, 2) , np.arctan2(T.item(1, 0), T.item(0, 0))])
+    return np.array([T.item(0, 2), T.item(1, 2), np.arctan2(T.item(1, 0), T.item(0, 0))])
+
 
 def chord(th):
     """
     Returns the cord function of the given angle
     """
     return 2*np.sin(th/2)
+
+
+def norm_pi(th):
+    """
+    Normalizes the given angle (forces it to be between 0 and -pi)
+    """
+    while th > np.pi:
+        th -= 2 * np.pi
+    while th < -np.pi:
+        th += 2 * np.pi
+    return th
