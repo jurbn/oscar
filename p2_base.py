3bb9abe3e314404c65ba6ebfb8390e4511efa3f0
jurbn <777295@unizar.es>
2022-10-06 18:02:12 +0000

    awa

--- p2_base.py
+++ p2_base.py
@@ -4,55 +4,23 @@ import argparse
 import numpy as np
 import time
 from Robot import Robot
-
+import movement as mv
 
 def main(args):
     try:
         if args.radioD < 0:
             print('d must be a positive value')
             exit(1)
-
-        # Instantiate Odometry. Default value will be 0,0,0
-        # robot = Robot(init_position=args.pos_ini)
         oscar = Robot()
-
-        print("X value at the beginning from main X= %.2f" %(oscar.x.value))
-
-        # 1. launch updateOdometry Process()
+        oscar.setup()
         oscar.startOdometry()
-
-        # 2. perform trajectory
-
-
-        # DUMMY CODE! delete when you have your own
-        oscar.setSpeed(1,1)
-        print("Start : %s" % time.ctime())
-        time.sleep(3)
-        print("X value from main tmp %d" % oscar.x.value)
-        time.sleep(3)
-        print("End : %s" % time.ctime())
-
-        oscar.lock_odometry.acquire()
-        print("Odom values at main at the END: %.2f, %.2f, %.2f " % (oscar.x.value, oscar.y.value, oscar.th.value))
-        oscar.lock_odometry.release()
-
-        # PART 1:
-        # oscar.setSpeed()
-        # until ...
-
-        # PART 2:
-        # oscar.setSpeed()
-        # until ...
-
-        # ...
-
-
-
-        # 3. wrap up and close stuff ...
-        # This currently unconfigure the sensors, disable the motors,
-        # and restore the LED to the control of the BrickPi3 firmware.
-        oscar.stopOdometry()
-
+        #oscar.setSpeed(10, 0)
+        #time.sleep(5)
+        #oscar.setSpeed(0, 10)
+        #time.sleep(5)
+        #oscar.stopOdometry()
+        #oscar.stopRobot()      
+        mv.eight(oscar, 0.2, 1)
 
     except KeyboardInterrupt:
     # except the program gets interrupted by Ctrl+C on the keyboard.
