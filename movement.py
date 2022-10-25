15ab5e7fa0ffb1bb3dc7c9a559ff13cf9ae37bc8
jurbn <777295@unizar.es>
2022-10-25 18:12:15 +0200

    me aburro en la biblio

--- movement.py
+++ movement.py
@@ -78,21 +78,40 @@ def slalom(robot, r1=0.1, r2=0.2, d=1, v=0.2):
     Does a odometry-based slalom with the given r1, r2, diameter and linear speed.\n r1 must be greater than r2.
     """
     #robot.logger.info('Starting an odometry-based slalom.\n Initial position: ({}, {}, {})'.format(robot.x.value, robot.y.value, robot.th.value))
+    logging.info('Starting the slalom...')
     w1 = v / r1
     w2 = v / r2
-    th = (np.pi/2) - math.acos((r2-r1)/d)
-    th1 = (np.pi/2 - th)    # angle to turn on the first circle (twice: while leaving and when arriving)
-    th2 = 2*(np.pi/2 + th)
-    o = math.sqrt(math.pow(d, 2)+math.pow(r2-r1, 2))  # distance to be covered in between circles
-    while(robot.th.value > -math.pi/2):
+    th = math.abs(math.acos((r2-r1)/d)) # we abs it so we have a first cuadrant angle
+    tx1 = r1-r1*math.sin(th)
+    ty1 = r1*math.cos(th)
+    tx2 = r1+d-r2*math.sin(th)
+    ty2 = r2*math.cos(th)
+    t11 = np.array([tx1, ty1])
+    t12 = np.array([tx1, -ty2])
+    t21 = np.array([tx2, ty2])
+    t22 = np.array([tx2, -ty2])
+
+    logging.info('Spinning:')
+    while(robot.th.value < -math.pi/2):
         robot.setSpeed(0, -w1)
-        print('mediavuelta')
-    while(robot.th.value < th1):
+    logging.info('primer tramo!')
+    while(robot.th.value <  -th):
         robot.setSpeed(v, w1)
-        print('primer chercolo')
-    while(not sage.is_near(robot,np.array([r1*(1-math.tan(th))+d/math.cos(th), r2-r1*(1-math.cos(th))]),0.05)):
+    logging.info('amo to recto')
+    while(not sage.is_near(robot, t21, 0.05)):
         robot.setSpeed(v,0)
-        print('amo to recto')
+    logging.info('y giramo')
+    while(robot.th.value > math.pi-th):
+        robot.setSpeed(v, w2)
+    logging.info('giro terminao, to recto')
+    while(not sage.is_near(robot, t12, 0.05)):
+        robot.setSpeed(v,0)
+    logging.info('empezamo el ultimo giro')
+    while(robot.th.value < math.pi):    # primera parte por lo de la normalización (pasa de pi a -p)
+        robot.setSpeed(v, w1)
+    while(robot.th.value < -math.pi/2): # vamos de -pi a -pi/2
+        robot.setSpeed(v, w1)
+    logging.info('Finished the circuit!!!')
     robot.setSpeed(0,0)    
 
 ################
