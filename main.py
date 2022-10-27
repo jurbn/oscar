6773ccc28b3c3f7d092f1b83820b71278222079b
beiwis <777184@unizar.es>
2022-10-27 15:35:46 +0200

    main(function) jejej

--- main.py
+++ main.py
@@ -7,23 +7,47 @@ import movement as mv
 import logging
 import sage
 
-def main():
-    try:
+def main(function=''):
+    try:        
+        #########################
+        #     initialisation    #
+        #########################
+
         logging.basicConfig(filename='log/' + time.strftime('%y-%m-%d--%H:%M:%S') + '.log', level=logging.DEBUG)
         logging.getLogger().addHandler(logging.StreamHandler())
         logging.info('Program started')
         oscar = Robot()
         oscar.setup()
         logging.info('Initial location: {}, {}, {}'.format(oscar.x.value, oscar.y.value, oscar.th.value))
+        
+        #########################
+        #   various functions   #
+        #########################
+
         #sage.plot_animation(oscar)
-        #mv.eight(oscar)
-        #mv.spin(oscar, 3.1416, 5)
-        #oscar.setSpeed(0, 0)
-        #mv.slalom(oscar)
-        oscar.trackObject()
+        if function == 'eight':
+            mv.eight(oscar)
+        elif function == 'slalom' :
+            mv.slalom(oscar)
+        elif function == 'spin' :
+            mv.spin(oscar, 3.1416, 5)
+        elif function == 'run' :
+            mv.run(oscar, 0.25, 5)
+        elif function == 'stop' :
+            oscar.setSpeed(0, 0)
+        elif function == 'track' :
+            oscar.trackObject()
+        
+        #########################
+        #      closing up       #
+        #########################
+
         oscar.stopOdometry()
         sage.plot_file(oscar.odometry_file)
-        
+
+    
+    # In the event of a keyboard interruption, we stop the robot's movement 
+    # and close the odometry process, as well as log the event as a warning.    
     except KeyboardInterrupt:
         logging.warning('A keyboard interruption was detected!')
         mv.abrupt_stop(oscar)
